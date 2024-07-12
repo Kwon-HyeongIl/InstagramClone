1580ad55982c3b1d378f3c091407087ab659846a
khi <llioopv@naver.com>
2024-07-12 16:40:48 +0900

    Initial Commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by 권형일 on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by 권형일 on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
